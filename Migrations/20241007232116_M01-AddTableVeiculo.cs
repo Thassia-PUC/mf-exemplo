5d743a1cdfb67047d5365d572d8b4d166b84c577
Thassia-PUC <tdrumond@sga.pucminas.br>
2024-10-07 20:28:07 -0300

    Config BD tabela veiculo

---
+++ Migrations/20241007232116_M01-AddTableVeiculo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_exemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01AddTableVeiculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veículoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnoFabricacao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnoModelo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veículoas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veículoas");
+        }
+    }
+}
---
+++ Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_exemplo.Models
+{
+    [Table("Veículoas")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a Placa!")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        public string AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        public string AnoModelo { get; set; }
+
+
+
+    }
+}
